9b668ef7cc8931628da05f60dc0a97eb009e06ed
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-08-11 22:59:56 -0300

    Adicionando cabeçalho de biblioteca de utilidades de esfera

---
+++ lista3/bibliotecasAtividade1/Respostas/Caio/esfera_utils.h
@@ -0,0 +1,22 @@
+#ifndef _ESFERA_UTILS_H
+#define _ESFERA_UTILS_H
+
+#define PI 3.14
+
+/**
+ * @brief Calcula o volume de uma esfera;
+ * 
+ * @param R Raio da esfera
+ * @return float Volume da esfera;
+ */
+float calcula_volume(float R);
+
+/**
+ * @brief Calcula a área da superfície de uma esfera;
+ * 
+ * @param R Raio da esfera;
+ * @return float Área da superficie da esfera;
+ */
+float calcula_area(float R);
+
+#endif
