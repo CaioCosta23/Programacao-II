9db43204fbbbcd567e96171483fca256daf0c44c
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-08-15 12:33:15 -0300

    Inserindo comentários de documentação do arquivo de declaração de funções da biblioteca

--- lista3/bibliotecasAtividade2/Respostas/Caio/matrix_utils.h
+++ lista3/bibliotecasAtividade2/Respostas/Caio/matrix_utils.h
@@ -2,107 +2,107 @@
 #define MATRIX_UTILS_H
 
 /**
- * @brief 
+ * @brief Lê dos dados (elementos) de uma matriz;
  * 
- * @param rows 
- * @param cols 
- * @param matrix 
+ * @param rows Quantidade de linhas da matriz;
+ * @param cols Quantidade de colunas da matriz;
+ * @param matrix Matriz a ser lida;
  */
 void matrix_read(int rows, int cols, int matrix[rows][cols]);
 
 /**
- * @brief 
+ * @brief Imprime (os dados de) uma matriz;
  * 
- * @param rows 
- * @param cols 
- * @param matrix 
+ * @param rows Quantidade de linhas da matriz;
+ * @param cols Quantidade de colunas da matriz;
+ * @param matrix Matriz a ser imprimida;
  */
 void matrix_print(int rows, int cols, int matrix[rows][cols]);
 
 /**
- * @brief 
+ * @brief Verifica se é possível somar uma matriz com outra;
  * 
- * @param rows1 
- * @param cols1 
- * @param rows2 
- * @param cols2 
- * @return int 
+ * @param rows1 Quantidade de linhas da matriz 1;
+ * @param cols1 Quantidade de colunas da matriz 1;
+ * @param rows2 Quantidade de linhas da matriz 2;
+ * @param cols2 Quantidade de colunas da matriz 2;
+ * @return int 1 (verdadeiro) se for possível somar um matriz com outra, ou 0 (falso), caso contrário;
  */
 int possible_matrix_sum(int rows1, int cols1, int rows2, int cols2);
 
 /**
- * @brief 
+ * @brief Verifica se é possível subtrair uma matriz de outra;
  * 
- * @param rows1 
- * @param cols1 
- * @param rows2 
- * @param cols2 
- * @return int 
+ * @param rows1 Quantidade de linhas da matriz 1;
+ * @param cols1 Quantidade de colunas da matriz 1;
+ * @param rows2 Quantidade de linhas da matriz 2;
+ * @param cols2 Quantidade de colunas da matriz 2;
+ * @return int 1 (verdadeiro), se for possível subtrair uma matriz de outra, ou 0 (falso), caso contrário;
  */
 int possible_matrix_sub(int rows1, int cols1, int rows2, int cols2);
 
 /**
- * @brief 
+ * @brief Verifica se é possível multiplicar uma matriz por outra;
  * 
- * @param cols1 
- * @param rows2 
- * @return int 
+ * @param cols1 Quantidade de colunas da matriz 1;
+ * @param rows2 Quantidade de linhas da matriz 2;
+ * @return int 1 (verdadeiro) se for possível multiplicar uma matriz com outra, ou 0 (falso), caso contrário;
  */
 int possible_matrix_multiply(int cols1, int rows2);
 
 /**
- * @brief 
+ * @brief Soma uma matriz com outra;
  * 
- * @param rows1 
- * @param cols1 
- * @param matrix1 
- * @param rows2 
- * @param cols2 
- * @param matrix2 
+ * @param rows1 Quantidade de linhas da matriz 1;
+ * @param cols1 Quantidade de colunas da matriz 1;
+ * @param matrix1 Matriz 1;
+ * @param rows2 Quantidade de linhas da matriz 2;
+ * @param cols2 Quantidade de colunas da matriz 1;
+ * @param matrix2 Matriz 2;
  */
 void matrix_add(int rows1, int cols1, int matrix1[rows1][cols1], int rows2, int cols2, int matrix2[rows2][cols2]);
 
 /**
- * @brief 
+ * @brief Subtrai uma matriz de outra;
  * 
- * @param rows1 
- * @param cols1 
- * @param matrix1 
- * @param rows2 
- * @param cols2 
- * @param matrix2 
+ * @param rows1 Quantidade de linhas da matriz 1;
+ * @param cols1 Quantidade de colunas da matriz 1;
+ * @param matrix1 Matriz 1;
+ * @param rows2 Quantidade de linhas da matriz 2;
+ * @param cols2 Quantidade de colunas da matriz 2;
+ * @param matrix2 Matriz 2;
  */
 void matrix_sub(int rows1, int cols1, int matrix1[rows1][cols1], int rows2, int cols2, int matrix2[rows2][cols2]);
 
 /**
- * @brief 
+ * @brief Multiplica uma matriz por outra;
  * 
- * @param rows1 
- * @param cols1 
- * @param matrix1 
- * @param rows2 
- * @param cols2 
- * @param matrix2 
+ * @param rows1 Quantidade de linhas da matriz 1;
+ * @param cols1 Quantidade de colunas da matriz 1;
+ * @param matrix1 Matriz 1;
+ * @param rows2 Quantidade de linhas da matriz 2;
+ * @param cols2 Quantidade de colunas da matriz 2;
+ * @param matrix2 Matriz 2;
  */
 void matrix_multiply(int rows1, int cols1, int matrix1[rows1][cols1], int rows2, int cols2,int matrix2[rows2][cols2]);
 
 /**
- * @brief 
+ * @brief Transforma uma matriz em sua respectiva matriz transposta;
  * 
- * @param rows 
- * @param cols 
- * @param matrix 
- * @param result 
+ * @param rows Quantidade de linhas da matriz;
+ * @param cols Quantidade de colunas da matriz;
+ * @param matrix Matriz a ser transposta;
+ * @param result Matriz transposta;
  */
 void transpose_matrix(int rows, int cols, int matrix[rows][cols], int result[cols][rows]);
 
 /**
- * @brief 
+ * @brief Multiplica uma matriz (nesse caso, todos os seus elementos) por um 'escalar' (valor constante);
  * 
- * @param rows 
- * @param cols 
- * @param matrix 
- * @param scalar 
+ * @param rows Quantidade de linhas da matriz;
+ * @param cols Quantidade de colunas da matriz;
+ * @param matrix Matriz que será multiplicada (neste caso, todos os seus elementos);
+ * @param scalar Escalar (valor constante) pelo qual será multiplicada a matriz (seus elementos);
  */
 void scalar_multiply(int rows, int cols, int matrix[rows][cols], int scalar);
 
