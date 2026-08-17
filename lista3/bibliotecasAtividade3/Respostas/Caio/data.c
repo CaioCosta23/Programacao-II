0bc366679211349a87f122eff1b120929d9d4e48
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-08-17 17:26:04 -0300

    Corrigindo função de impressão de data

--- lista3/bibliotecasAtividade3/Respostas/Caio/data.c
+++ lista3/bibliotecasAtividade3/Respostas/Caio/data.c
@@ -158,7 +158,7 @@ void imprimeMesExtenso(int mes) {
 }
 
 void imprimeDataExtenso(int dia, int mes, int ano) {
-    printf("%02d de ");
+    printf("%02d de ", dia);
     imprimeMesExtenso(mes);
-    printf(" de %04d/n");
+    printf(" de %04d/n", ano);
 }
