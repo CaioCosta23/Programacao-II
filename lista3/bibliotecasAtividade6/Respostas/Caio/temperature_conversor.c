ea28c995c968e25ef2d83b62112000e538d4c7d4
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-08-19 15:10:48 -0300

    Corrigindo funções de conversão entre kelvin e Celsius (vice versa)

--- lista3/bibliotecasAtividade6/Respostas/Caio/temperature_conversor.c
+++ lista3/bibliotecasAtividade6/Respostas/Caio/temperature_conversor.c
@@ -3,8 +3,8 @@
 
 #include "temperature_conversor.h"
 
-#define MARCO_ESCALA_KELVIN 273
-#define MARCA_ESCALA_FAHRENHEIT 32
+#define MARCO_ESCALA_KELVIN 273.15
+#define MARCA_ESCALA_FAHRENHEIT 32.00
 
 float convert_celsius_to_kelvin(float temperature) {
     return temperature + MARCO_ESCALA_KELVIN;
@@ -19,21 +19,15 @@ float convert_kelvin_to_celsius(float temperature) {
 }
 
 float convert_kelvin_to_fahrenheit(float temperature) {
-    return convert_temperature(temperature, convert_fahrenheit_to_celsius);
+    return convert_celsius_to_fahrenheit(convert_kelvin_to_celsius(temperature));
 }
 
 float convert_fahrenheit_to_celsius(float temperature) {
     return ((temperature - MARCA_ESCALA_FAHRENHEIT) * ((float)5 / (float)9));
 }
 
-/**
- * @brief Converte uma temperatura de Fahreinheit para Kelvin;
- * 
- * @param temperature Temperatura a ser convertida;
- * @return float Temperatura convertida de uma escala de temperatura (Fahrenheit) para outra (Kelvin);
- */
 float convert_fahrenheit_to_kelvin(float temperature) {
-    return convert_temperature(temperature, convert_fahrenheit_to_celsius);
+    return convert_celsius_to_kelvin(convert_fahrenheit_to_celsius(temperature));
 }
 
 float convert_temperature(float temperature, float convert_func(float temperature)) {
