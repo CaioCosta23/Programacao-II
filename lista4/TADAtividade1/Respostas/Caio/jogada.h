754c21ff7043563fcdaaaf3211c3e7df801a1298
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-08-24 14:51:56 -0300

    Adicionando arquivo com declaração de bibloteca

---
+++ lista4/TADAtividade1/Respostas/Caio/jogada.h
@@ -0,0 +1,45 @@
+#ifndef _JOGADA_H
+#define _JOGADA_H
+
+typedef struct {
+    int x, y;
+    int sucesso; // Variável lógica;
+}tJogada;
+
+
+/**
+ * @brief Lê uma jogada do jogo;
+ * 
+ * @return tJogada Tipo Abstrato de Dado (T.A.D) estruturado que contém os dados (inicializados/atualizados) da jogo;
+ */
+tJogada LeJogada();
+
+/**
+ * @brief 
+ * 
+ * @param jogada tJogada Tipo Abstrato de Dado (T.A.D) estruturado que contém os atuais dados do tabuleiro do jogo;
+ * @return int valor do da coordenada X
+ */
+int ObtemJogadaX(tJogada jogada) {
+    return jogada.x;
+}
+
+/**
+ * @brief Busca/Obtém o valor da coordenada Y no tabuleiro;
+ * 
+ * @param jogada tJogada Tipo Abstrato de Dado (T.A.D) estruturado que contém os atuais dados do tabuleiro do jogo;
+ * @return int valor de coordenda do jogo
+ */
+int ObtemJogadaY(tJogada jogada) {
+    return jogada.x;
+}
+
+/**
+ * @brief Verifica se uma jogada foi bemsucedida;
+ * 
+ * @param jogada tJogada Tipo Abstrato de Dado (T.A.D) estruturado que contém os atuais dados do tabuleiro do jogo;
+ * @return int 1 (verdadeiro) se a jogada for bem sucedida, ou 0 (casonrário);
+ */
+int FoiJOgadaBemSucedida(tJogada jogada);
+
+#endif
