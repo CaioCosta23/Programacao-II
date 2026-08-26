ba5dada46d5bbcc9f3dadaae2b1fbf45273cd904
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-08-26 11:35:48 -0300

    Adicionando arquivo de biblioteca de conta

---
+++ lista4/TADAtividade5/Respostas/Caio/conta.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "conta.h"
+
+tConta CriaConta(int numero, tUsuario user) {
+    tConta conta;
+
+    conta.numero = numero;
+    conta.saldo = 0;
+    conta.user = user;
+
+    return conta;
+}
+
+int VerificaConta(tConta conta, int numero) {
+    return (conta.numero == numero);
+}
+
+tConta SaqueConta(tConta conta, float valor) {
+    conta.saldo -= valor;
+
+    return conta;
+}
+
+tConta DepositoConta(tConta conta, float valor) {
+    conta.saldo += valor;
+
+    return conta;
+}
+
+void ImprimeConta(tConta conta) {
+    printf("Conta: %d\n", conta.numero);
+    printf("Saldo: R$ %.2f\n", conta.saldo);
+    ImprimeUsuario(conta.user);
+}
