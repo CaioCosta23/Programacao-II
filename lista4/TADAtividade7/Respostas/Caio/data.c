177c6abfaa683e4497e8708c3df6281610679e04
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-08-28 10:52:13 -0300

    Adicionando função auxiliar de avançar data para calcula da diferença de dias entre uma data e outra

--- lista4/TADAtividade7/Respostas/Caio/data.c
+++ lista4/TADAtividade7/Respostas/Caio/data.c
@@ -18,6 +18,27 @@
 #define NOVEMBRO 11
 #define DEZEMBRO 12
 
+/**
+ * @brief Avança os dias de uma data (dia, mês e ano se necessários); 
+ * 
+ * @param data Tipo de Dados Abstratos (T.A.D.) que representa a estrutura que contém os dados (atualizados) da data;
+ * @return tData Tipo de Dados Abstratos (T.A.D.) que representa a estrutura que contém os dados (atualizados) da data após a mesma ter seu dia (e mês e ano, caso necessário) avançados;
+ */
+static tData AvancaDiaData(tData data) {
+    if (data.dia == NumeroDiasMes(data)) {
+        data.dia = 1;
+        if (data.mes == NUMERO_MESES_ANO) {
+            data.mes = 1;
+            data.ano += 1;
+        }else {
+            data.mes += 1;
+        }
+    }else {
+        data.dia += 1;
+    }
+    return data;
+}
+
 tData CriaData(int dia, int mes, int ano) {
     tData data;
 
@@ -85,7 +106,6 @@ int CalculaDiferencaDias(tData data1, tData data2) {
         while((data1.dia != data2.dia) || (data1.mes != data2.mes) || (data1.ano != data2.ano)) {
             diferenca += 1;
         }
-    
     }else {
         /*
          * Neste caso, não há necessidade de preocupação de tamanho (por atribuir um 'short int' a um 'int'), uma vez que sabemos que somente há
