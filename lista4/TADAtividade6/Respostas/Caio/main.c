144fae0656e76e589f239989bce69400f62ea746
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-09-05 17:31:28 -0300

    Corrigind leitura de opção de transpôr matriz, impressão de menu, atribuição de variáveis de matriz em multiplicar por escalar e adicionando opção de erro em adição subtração e multiplicação

--- lista4/TADAtividade6/Respostas/Caio/main.c
+++ lista4/TADAtividade6/Respostas/Caio/main.c
@@ -17,8 +17,8 @@
 void imprimeMenu() {
     printf("1 - Somar matrizes\n");
     printf("2 - Subtrair matrizes\n");
-    printf("3 - Multipĺicar matrizes\n");
-    printf("4 - Multiplicação de uma matriz por escalar\n");
+    printf("3 - Multiplicar matrizes\n");
+    printf("4 - Multiplicacao de uma matriz por escalar\n");
     printf("5 - Transposta de uma matriz\n");
     printf("6 - Encerrar o programa\n");
 
@@ -34,6 +34,7 @@ int main() {
     int quantidadeLinhas, quantidadeColunas;
     tMatrix matriz1, matriz2, resultante;
     unsigned short int opcao;
+    const unsigned short int OPCAO_MATRIZ_1  = 1, OPCAO_MATRIZ_2 = 2;
     
     scanf("%d %d\n", &quantidadeLinhas, &quantidadeColunas);
 
@@ -49,32 +50,53 @@ int main() {
         imprimeMenu();
 
         scanf("%hd\n", &opcao);
+        printf("\n");
 
         switch(opcao) {
             case SOMAR:
-                if (PossibleMatrixSum(matriz1, matriz2))
+                if (PossibleMatrixSum(matriz1, matriz2)) {
                     resultante = MatrixAdd(matriz1, matriz2);
                     MatrixPrint(resultante);
+                }else {
+                    printf("Erro: as dimensoes da matriz nao correspondem\n\n");
+                }
                     break;
             case SUBTRAIR:
-                if (PossibleMatrixSub(matriz1, matriz2))
+                if (PossibleMatrixSub(matriz1, matriz2)) {
                     resultante = MatrixSub(matriz1, matriz2);
                     MatrixPrint(resultante);
+                }else {
+                    printf("Erro: as dimensoes da matriz nao correspondem\n\n");
+                }
                     break;
             case MULTIPLICAR:
-                if (PossibleMatrixMultiply(matriz1, matriz2))
+                if (PossibleMatrixMultiply(matriz1, matriz2)) {
                     resultante = MatrixMultiply(matriz1, matriz2);
                     MatrixPrint(resultante);
+                }else {
+                    printf("Erro: o numero de colunas da primeira matriz eh diferente do numero de linhas da segunda matriz\n\n");
+                }
                     break;
             case MULTIPLICACAO_ESCALAR:
                 int escalar;
+                unsigned short int opcaoMatriz;
 
-                scanf("%d\n", &escalar);
-                resultante = MatrixMultiplyByScalar(matriz1, escalar);
-                MatrixPrint(resultante);
+                scanf("%d %hd\n", &escalar, &opcaoMatriz);
+
+                if (opcaoMatriz == OPCAO_MATRIZ_1) {
+                    matriz1 = MatrixMultiplyByScalar(matriz1, escalar);
+                    MatrixPrint(matriz1);
+                }else if (opcaoMatriz == OPCAO_MATRIZ_2) {
+                    matriz2 = MatrixMultiplyByScalar(matriz2, escalar);
+                    MatrixPrint(matriz2);
+                }else {
+                    printf("Opcao invalida de matriz.\n");
+                }
                 break;
             case TRANSPOSTA:
                 resultante = TransposeMatrix(matriz1);
+                MatrixPrint(resultante);
+                resultante = TransposeMatrix(matriz2);
                 MatrixPrint(resultante);
                 break;
             case ENCERRAR:
