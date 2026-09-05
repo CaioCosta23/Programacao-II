200f44e83f46d222f490e0541a3fd50150bd42e3
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-09-05 11:20:24 -0300

    Adicionando arquivo principal

---
+++ lista4/TADAtividade8/Respostas/Caio/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "departamento.h"
+
+tDepartamento leDepartamento() {
+    char curso1[STRING_MAX], curso2[STRING_MAX], curso3[STRING_MAX], nome[STRING_MAX], diretor[STRING_MAX];
+    int media1, media2, media3;
+
+    scanf("%[^\n]\n", nome);
+    scanf("%[^\n]\n", diretor);
+    scanf("%[^\n]\n", curso1);
+    scanf("%[^\n]\n", curso2);
+    scanf("%[^\n]\n", curso3);
+    scanf("%d %d %d\n", &media1, &media2, &media3);
+
+    return criaDepartamento(curso1, curso2, curso3, nome, media1, media2, media3, diretor);
+}
+
+/**
+ * @brief Programa que captura os dados de departamentos, analisa-os e exibe-os na tela;
+ * 
+ * @return int Programa principal;
+ */
+int main() {
+    int quantidadeDepartamentos, d;
+    
+    scanf("%d", &quantidadeDepartamentos);
+
+    tDepartamento departamentos[quantidadeDepartamentos];
+
+    for(d = 0; d < quantidadeDepartamentos; d++) {
+        departamentos[d] = leDepartamento();
+    }
+
+    oredenaPorMediaDepartamentos(departamentos, quantidadeDepartamentos);
+
+    for(d = 0; d < quantidadeDepartamentos; d++) {
+        imprimeAtributosDepartamento(departamentos[d]);
+    }
+
+    return 0;
+}
