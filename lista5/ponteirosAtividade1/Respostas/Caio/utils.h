d05f505c0dae3e005e9ccb19d4b05cef41c840a5
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-09-07 20:52:26 -0300

    Adicionando arquivo de declaração de biblioteca

---
+++ lista5/ponteirosAtividade1/Respostas/Caio/utils.h
@@ -0,0 +1,16 @@
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+/**
+ * @brief Troca os valores de duas variáveis (e retorna um valor que indica se atroca foi bem-sucedida ou não);
+ * 
+ * @param n1 Ponteiro para o rimeiro número;
+ * @param n2 Ponteiro para o segundo número;
+ * @return int 1 (verdadeiro) se a troca foi realizada com sucesso ou 0 (falso), caso contrário;
+ * 
+ * @OBS: Aparentemente, na questão original a função foi escrita com o nome errado, então deverár ficar assim para o
+ * script de correção rodar de maneira correta (baseado nos arquivos-objetos da pasta de gabarito)
+ */
+int TrocaInterios(int *n1, int *n2);
+
+#endif
