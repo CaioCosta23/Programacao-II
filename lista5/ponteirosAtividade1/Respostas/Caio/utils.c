0118c9b0cf1bcd3420005a129db7316c3a801577
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-09-07 21:16:59 -0300

    Adicionando lógica de troca de conteúdo de ponteiros

--- lista5/ponteirosAtividade1/Respostas/Caio/utils.c
+++ lista5/ponteirosAtividade1/Respostas/Caio/utils.c
@@ -4,5 +4,12 @@
 #include "utils.h"
 
 int TrocaInterios(int *n1, int *n2) {
-    return ((*n1) <= (*n2));
+    if ((*n1) > (*n2)) {
+        int auxiliar = *n1;
+        *n1 = *n2;
+        *n2 = auxiliar;
+
+        return 1;
+    }
+    return 0;
 }
