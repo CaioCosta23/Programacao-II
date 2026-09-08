85911b1040f1f0f1b5f35db9e6c6eafd201c260a
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-09-08 09:44:11 -0300

    Adicionando arquivo principal

---
+++ lista5/ponteirosAtividade2/Respostas/Caio/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "utils.h"
+
+/**
+ * @brief Programa que lê o número de intervalos e dois valores representando o início e o fim de um intervalo (respectivamente),
+ * verifica quais números desse intervalo são inteiros e imprime o maior e o menor número primo do intervalo;
+ * 
+ * @return int Programa principal;
+ */
+int main(){
+    int casos;
+
+    scanf("%d\n", &casos);
+
+    while(casos) {
+        int m, n, menor, maior;
+
+        LeIntervalor(&m, &n);
+
+        ObtemMaiorEMenorPrimo(m, n, &menor, &maior);
+
+        printf("%d %d\n", menor, maior);
+
+        casos--;
+    }
+
+    return 0;
+}
