402a0f880c2283b1d85ad9ec3250caa4791f3035
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-09-14 14:21:58 -0300

    Adicionando arquivo de biblioteca de utilidades

---
+++ lista6/Parte_1/alocacaoAtiividade1/Respostas/Caio/utils.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int *CriaVetor(int tamanho) {
+    int *vetor = NULL;
+
+    vetor = (int*)malloc(tamanho * sizeof(int));
+
+    if (vetor == NULL)
+        printf("Erro! Alocacao de memoria em craicao de vetor, mal sucedida.\n");
+
+    return vetor;
+}
+
+void LeVetor(int *vetor, int tamanho) {
+    int v;
+
+    for(v = 0; v < tamanho; v++) 
+        scanf("%d", *(vetor + v));
+}
+
+float CalculaMedia(int *vetor, int tamanho) {
+    int v;
+    int soma = 0;
+
+    for(v = 0; v < tamanho; v++)
+        soma += *(vetor + v);
+
+    return (float)soma / (float)tamanho;
+}
+
+void LiberaVetor(int *vetor) {
+    free(vetor);
+}
