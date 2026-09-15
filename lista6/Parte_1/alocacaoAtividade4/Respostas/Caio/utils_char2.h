69844e2922d0fdfc18bd0d66487d5d55bdee0cd8
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-09-15 12:57:32 -0300

    Corrigindo parâmetros de função

--- lista6/Parte_1/alocacaoAtividade4/Respostas/Caio/utils_char2.h
+++ lista6/Parte_1/alocacaoAtividade4/Respostas/Caio/utils_char2.h
@@ -17,16 +17,16 @@ char *CriaVetorTamPadrao();
  * @param tamanho Tamanho do vetor/lista/'array';
  * @return char* Ponteiro para o vetor/lista/'array' com o tamanho aumentado (realocado dinamicamente - na memória);
  */
-char *AumentaTamanhoVetor(char *vetor, int tamanho);
+char *AumentaTamanhoVetor(char *vetor, int tamanhoantigo);
 
 /**
  * @brief Lê os dados do vetor/lista/'array' de caracteres;
  * 
  * @param vetor Ponteiro para o vetor/lista/'array' de caracteres;
- * @param tamanho Tamanho do vetor/lista/'array' de caracteres;
+ * @param tamanho Ponteiro para o amanho do vetor/lista/'array' de caracteres;
  * @return char* Ponteiro para o vetor/lista/array de caracteres, com o mesmo preenchido com os dados lidos;
  */
-char *LeVetor(char *vetor, int tamanho);
+char *LeVetor(char *vetor, int *tamanho);
 
 /**
  * @brief Imprime o conteúdo de uma 'string' (vetor/lista/'array' de caracteres);
