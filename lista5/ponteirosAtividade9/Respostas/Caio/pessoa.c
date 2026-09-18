dc25998d6c7fc6b0c15c47aca05c8a227926136d
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-09-18 11:29:43 -0300

    Corrigindo lnome de função de associar familiares, lógica da função da mesma, inserindo leitura de quebra de linha na leitura dos nomes e incrementando impressão de resultado (com pais não informados)

--- lista5/ponteirosAtividade9/Respostas/Caio/pessoa.c
+++ lista5/ponteirosAtividade9/Respostas/Caio/pessoa.c
@@ -4,24 +4,12 @@
 #include "pessoa.h"
 
 /**
- * @brief Verifica se uma pessoa existe no vetor/lista/'array' de pessoas;
- * 
- * @param indicePessoa Índice da posição da pessoa na llista/vetor/'array' de pessoas;
- * @return unsigned short int 1(verdadeiro) se a pesoa existir dentro da lista/vetor/'array' de pessoas ou 0 (falso), caso contrário;
- * 
- * @OBS: O código do arquivo em si, sempre irá supor que o índice nunca será maior que o tamanho da lista/vetor/'array' de pessoas;
- */
-static unsigned short int verificaExistenciaPessoa(unsigned int indicePessoa) {
-    return (indicePessoa > -1);
-}
-
-/**
  * @brief Imprime o nome de uma pessoa;
  * 
  * @param pessoa Ponteiro para Tipo Abstrato de Dados (T.A.D.) que representa a estrutura que contém as informações de uma pessoa (com os dados atualizados);
  */
 static void imprimeNome(tPessoa *pessoa) {
-    printf("%s", (*pessoa).nome);
+    printf("%s\n", (*pessoa).nome);
 }
 
 tPessoa CriaPessoa() {
@@ -36,38 +24,50 @@ tPessoa CriaPessoa() {
 
 
 void LePessoa(tPessoa *pessoa) {
-    scanf("%[^\n]\n", pessoa->nome);
+    scanf("\n%[^\n]\n", pessoa->nome);
 }
 
-int VerificaSeTemPaiPessoa(tPessoa *pessoa) {
+int VerificaSeTemPaisPessoa(tPessoa *pessoa) {
     return (((*pessoa).pai != NULL) || ((*pessoa).mae != NULL));
 }
 
 
-void AssociaFamiliasGrupoPessoas(tPessoa *pessoas) {
-    int unsigned indiceMae, indicePai, indiceFilho;
+void AssociaFamiliasGruposPessoas(tPessoa *pessoas) {
+    int quantidadeAssociacoes, a, indiceMae, indicePai, indiceFilho;
 
-    scanf("mae: %d, pai: %d, filho: %d\n", &indiceMae, &indicePai, &indiceFilho);
+    scanf("%d\n", &quantidadeAssociacoes);
 
-    if (verificaExistenciaPessoa(indiceMae))
-        (*(pessoas + indiceFilho)).mae = (pessoas + indiceMae);
-    if (verificaExistenciaPessoa(indicePai))
-        (*(pessoas + indiceFilho)).pai = (pessoas + indicePai);
+    for(a = 0; a < quantidadeAssociacoes; a++) {
+        scanf("mae: %d, pai: %d, filho: %d\n", &indiceMae, &indicePai, &indiceFilho);
+
+        if (indiceFilho > -1) {
+            if (indiceMae > -1)
+                (*(pessoas + indiceFilho)).mae = (pessoas + indiceMae);
+            
+            if (indicePai > -1)
+                (*(pessoas + indiceFilho)).pai = (pessoas + indicePai);
+        }
+    }
 }
 
 
 void ImprimePessoa(tPessoa *pessoa) {
-    if (VerificaSeTemPaiPessoa(pessoa)){
+    if (VerificaSeTemPaisPessoa(pessoa)){
         printf("NOME COMPLETO: ");
         imprimeNome(pessoa);
 
         if ((*pessoa).pai != NULL) {
             printf("PAI: ");
             imprimeNome((*pessoa).pai);
+        }else {
+            printf("PAI: NAO INFORMADO\n");
         }
         if ((*pessoa).mae != NULL) {
             printf("MAE: ");
             imprimeNome((*pessoa).mae);
+        }else {
+            printf("MAE: NAO INFORMADO\n");
         }
+        printf("\n");
     }
 }
