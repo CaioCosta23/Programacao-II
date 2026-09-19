fe2e9af8620b3c6993a5de704c6cf3185c238fed
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-09-19 14:52:17 -0300

    Inserindo função de criar e função de ler matriz, corrigindo impressão de matriz transposta e incrementando liberação de memória

--- lista6/Parte_1/alocacaoAtividade2/Respostas/Caio/utils.c
+++ lista6/Parte_1/alocacaoAtividade2/Respostas/Caio/utils.c
@@ -4,33 +4,62 @@
 #include "utils.h"
 
 int **CriaMatriz(int linhas, int colunas){
-    int **matriz;
+    int **matriz = NULL;
+    unsigned int l, c;
 
-    // A fazer;
+    matriz = (int**)malloc(linhas * sizeof(int*));
+
+    if (matriz == NULL) {
+        printf("Erro! Alocacao geral da matriz mal-sucedida.\n");
+        exit(1);
+    }
+
+    // Inicializando cada ponteiro de vetor da matriz;
+    for(l = 0; l < linhas; l++)
+        *(matriz + l) = NULL;
+    
+    for(l = 0; l < linhas; l++) {
+            *(matriz + l) = (int*)calloc(colunas, sizeof(int));
+            
+            if (*(matriz + l) == NULL) {
+                printf("Erro! Alocacao de memoria nas linhas da matriz mal sucedida.\n");
+                LiberaMatriz(matriz, linhas);
+                exit(1);
+            }
+    }
 
     return matriz;
 }
 
 void LeMatriz(int **matriz, int linhas, int colunas) {
-    // A fazer;
+    unsigned int l, c;
+
+    for(l = 0; l < linhas; l++) {
+        for(c = 0; c < colunas; c++) {
+            scanf("%d", ((*(matriz + l)) + c));
+        }
+    }
 }
 
 void ImprimeMatrizTransposta(int **matriz, int linhas, int colunas) {
     int l, c;
 
-    for(l = 0; l < linhas; l++) {
-        for(c = 0; c < colunas; c++) {
-            //printf("%d ", *(*(matriz + c)));
+    for(c = 0; c < colunas; c++) {
+        for(l = 0; l< linhas; l++) {
+            printf("%d ", *((*(matriz + l)) + c));
         }
         printf("\n");
     }
 }
 
 void LiberaMatriz(int **matriz, int linhas) {
-    int l;
-
-    for(l = 0; l < linhas; l++) {
-        free(matriz + l);
+    if (matriz != NULL) {
+        unsigned int l;
+        
+        for(l = 0; l < linhas; l++) {
+            if (*(matriz + l) != NULL)
+                free((*(matriz + l)));
+        }
+        free(matriz);
     }
-    free(matriz);
 }
