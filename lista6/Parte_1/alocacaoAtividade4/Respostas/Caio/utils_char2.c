f9bcc49da600cba9398786f1604503c1d1cddd22
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-09-19 17:08:05 -0300

    Adicionando funções de leitura e realocação de memória para aumento de tamanho de vetor

--- lista6/Parte_1/alocacaoAtividade4/Respostas/Caio/utils_char2.c
+++ lista6/Parte_1/alocacaoAtividade4/Respostas/Caio/utils_char2.c
@@ -3,52 +3,80 @@
 
 #include "utils_char2.h"
 
-/**
- * @brief Calcula o tamanho da do vetor/lista/'array' de caracteres;
- * 
- * @param vetor Ponteiro para o vetor/lista/'array' de caracteres;
- * @return unsigned int Tamanho do vetor/lista/'array' de caracteres;
- */
-static unsigned int calculaTamanhoString(char *vetor) {
-    unsigned int tamanho = 0;
-
-    while(*(vetor + tamanho) =! '\0')
-        tamanho++;
-    
-    return tamanho;
-}
-
+#define CARACTERE_PADRAO '_'
 
 char *CriaVetorTamPadrao() {
     char *vetor = NULL;
 
-    vetor = (char*)malloc(TAM_PADRAO * sizeof(char));
+    vetor = (char*)malloc((TAM_PADRAO + 1) * sizeof(char));
 
     if (vetor == NULL) {
         printf("Erro! Alocacao de memoria de vetor de caracteres mal-sucedida");
         exit(1);
     }
 
+    unsigned int v;
+
+    for(v = 0; v < TAM_PADRAO; v++)
+        *(vetor + v) = CARACTERE_PADRAO;
+
+    *(vetor + TAM_PADRAO) = '\0';
+
     return vetor;
 }
 
 
 char *AumentaTamanhoVetor(char *vetor, int tamanhoantigo) {
-    // Código à fazer;
+    unsigned int novoTamanho;
+
+    novoTamanho = tamanhoantigo + TAM_PADRAO;
+
+    vetor = (char*)realloc(vetor, (novoTamanho + 1) * sizeof(char));
+
+    if (vetor == NULL) {
+        printf("Erro! Realocacao de memoria do vetor mal-sucedida.\n");
+        LiberaVetor(vetor);
+        exit(1);
+    }
+
+    unsigned int v;
+
+    for(v = tamanhoantigo; v < novoTamanho; v++)
+        *(vetor + v) = CARACTERE_PADRAO;
+    
+    *(vetor + novoTamanho) = '\0';
 
     return vetor;
 }
 
 char *LeVetor(char *vetor, int *tamanho) {
-    // À fazer código;
+    int v = 0;
+    char letra;
+
+    while(1) {
+        scanf("%c", &letra);
+
+        if (letra == '\n') {
+            break;
+        }
+
+        if (v == *tamanho){
+            vetor = AumentaTamanhoVetor(vetor, *tamanho);
+            *tamanho = (*tamanho) + TAM_PADRAO;
+        }
+        
+        *(vetor + v) = letra;
+        v++;
+    }
 
     return vetor;
 }
 
 void ImprimeString(char *vetor) {
-    printf("%s", *vetor);
+    printf("%s\n", vetor);
 }
 
 void LiberaVetor(char *vetor) {
-    free(vetor);
+    if (vetor != NULL)
+        free(vetor);
 }
