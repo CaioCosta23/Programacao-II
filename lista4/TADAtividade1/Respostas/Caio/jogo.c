405ed72581d5d22e461083b689bf8c576b6b0144
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-09-21 20:46:27 -0300

    Corrigindo função de continuar jogo

--- lista4/TADAtividade1/Respostas/Caio/jogo.c
+++ lista4/TADAtividade1/Respostas/Caio/jogo.c
@@ -13,35 +13,6 @@ tJogo CriaJogo() {
     return jogo;
 }
 
-int ContinuaJogo() {
-    char resposta;
-
-    do {
-        printf("Jogar novamente? (s,n)\n");
-        scanf("%c\n", &resposta);
-    }while((resposta != 's') && (resposta != 'n'));
-
-    if (resposta == 's')
-        return 1;
-    else
-        return 0;
-}
-
-int AcabouJogo(tJogo jogo) {
-    if (!(TemPosicaoLivreTabuleiro(jogo.tabuleiro))) {
-        printf("Sem vencedor!\n");
-        return 1;
-    }else if (VenceuJogador(jogo.jogador1, jogo.tabuleiro)) {
-        pprintf("JOGADOR 1 Venceu\n");
-        return 1;
-    }else if (VenceuJogador(jogo.jogador2, jogo.tabuleiro)) {
-        printf("JOGADOR 2 Venceu\n");
-        return 1;
-    }else{
-        return 0;
-    }
-}
-
 void ComecaJogo(tJogo jogo) {
     unsigned short int contadorJogadas = 0;
 
@@ -52,6 +23,37 @@ void ComecaJogo(tJogo jogo) {
         if (contadorJogadas % 2 == 0)
             jogo.tabuleiro = JogaJogador(jogo.jogador1, jogo.tabuleiro);
         else
-            jogo.tabuleiro = JogaJogador(jogo.jogador1, jogo.tabuleiro);
+            jogo.tabuleiro = JogaJogador(jogo.jogador2, jogo.tabuleiro);
+
+        ImprimeTabuleiro(jogo.tabuleiro);
+
+        contadorJogadas++;
     }while(!(AcabouJogo(jogo)));
+}
+
+int ContinuaJogo() {
+    char resposta;
+
+    printf("Jogar novamente? (s,n)\n");
+
+    do {
+        scanf(" %c", &resposta);
+    } while (resposta != 's' && resposta != 'n');
+
+    return (resposta == 's');
+}
+
+int AcabouJogo(tJogo jogo) {
+     if (VenceuJogador(jogo.jogador1, jogo.tabuleiro)) {
+        printf("JOGADOR 1 Venceu!\n");
+        return 1;
+    }else if (VenceuJogador(jogo.jogador2, jogo.tabuleiro)) {
+        printf("JOGADOR 2 Venceu!\n");
+        return 1;
+    }else if (!(TemPosicaoLivreTabuleiro(jogo.tabuleiro))) {
+        printf("Sem vencedor!\n");
+        return 1;
+    }else{
+        return 0;
+    }
 }
