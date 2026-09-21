f7230ddde2e2a46ab53886aad5143c25b8e6bedb
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-09-21 17:15:26 -0300

    Adicionando arquivo de biblioteca de jogador

---
+++ lista4/TADAtividade1/Respostas/Caio/jogador.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "jogador.h"
+
+#include "jogada.h"
+
+tJogador CriaJogador(int idJogador) {
+    tJogador jogador;
+
+    jogador.id = idJogador;
+
+    return jogador;
+}
+
+tTabuleiro JogaJogador(tJogador jogador, tTabuleiro tabuleiro) {
+    tJogada jogada;
+
+    printf("Jogador %d\n", jogador.id);
+    printf("Digite uma posicao (x e y):\n");
+
+    jogada = LeJogada();
+    
+    if (FoiJogadaBemSucedida(jogada)) {
+        if (EhPosicaoValidaTabuleiro(ObtemJogadaX(jogada), ObtemJogadaY(jogada))){
+            if (TemPosicaoLivreTabuleiro(tabuleiro)) {
+                if (!(EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, ObtemJogadaX(jogada), ObtemJogadaY(jogada), jogador.id))) {
+                    tabuleiro = MarcaPosicaoTabuleiro(tabuleiro, jogador.id, ObtemJogadaX(jogada), ObtemJogadaY(jogada));
+                }else {
+                    printf("Posicao invalida (OCUPADA - [%d,%d] )!\n", ObtemJogadaX(jogada), ObtemJogadaY(jogada));
+                }
+            }
+        }else {
+            printf("Posicao invalida (FORA DO TABULEIRO - [%d %d] )!\n", ObtemJogadaX(jogada), ObtemJogadaY(jogada));
+        }
+    }
+    return tabuleiro;
+}
+
+int VenceuJogador(tJogador jogador, tTabuleiro tabuleiro) {
+    unsigned int l, c, dp, lds, cds;
+    unsigned short int coluna, linha, diagonalPrincipal, diagonalSecundaria;
+
+    linha = 1;
+    
+    for(l = 0; l < TAM_TABULEIRO; l++) {
+        coluna = 1;
+        for(c = 0; c < TAM_TABULEIRO; c++) {
+            // Verifica se a coluna está completa ou não
+            if (!(EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, l, c, jogador.id))) {
+                coluna = 0;
+                break;
+            }
+        }
+        if (coluna)
+            return 1;
+        // Verifica se a linha está completa ou não;
+        if (!(EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, l, c, jogador.id)))
+            linha = 0;
+    }
+    if (linha) 
+        return 1;
+    
+    diagonalPrincipal = 1;
+
+    for(dp = 0; dp < TAM_TABULEIRO; dp++) {
+        if (!(EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, dp, dp, jogador.id))) {
+            diagonalPrincipal = 0;
+            break;
+        }
+    }
+    if (diagonalPrincipal)
+        return 1;
+    
+    diagonalSecundaria = 1;
+    for(lds = 0, cds = (TAM_TABULEIRO - 1); lds < TAM_TABULEIRO, cds >= 0; lds++, cds--){
+        if (EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, lds, cds, jogador.id)) {
+            diagonalSecundaria = 0;
+            break;
+        }
+    }
+    // Como é a última verificação, podemos retornar diretamente;
+    return diagonalSecundaria;
+}
