99d45cae7bec1afd9e86f7b89c30b61a1694892d
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-09-21 18:43:36 -0300

    Arualizando função para que o jogador jogue

--- lista4/TADAtividade1/Respostas/Caio/jogador.c
+++ lista4/TADAtividade1/Respostas/Caio/jogador.c
@@ -17,18 +17,27 @@ tTabuleiro JogaJogador(tJogador jogador, tTabuleiro tabuleiro) {
     tJogada jogada;
 
     printf("Jogador %d\n", jogador.id);
-    printf("Digite uma posicao (x e y):\n");
 
     jogada = LeJogada();
     
     if (FoiJogadaBemSucedida(jogada)) {
         if (EhPosicaoValidaTabuleiro(ObtemJogadaX(jogada), ObtemJogadaY(jogada))){
-            if (TemPosicaoLivreTabuleiro(tabuleiro)) {
+            unsigned short int adversario;
+
+            if (jogador.id == PECA_1)
+                adversario = PECA_2;
+            else
+                adversario = PECA_1;
+            
+            if (!(EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, ObtemJogadaX(jogada), ObtemJogadaY(jogada), adversario))) {
                 if (!(EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, ObtemJogadaX(jogada), ObtemJogadaY(jogada), jogador.id))) {
+                    printf("Jogada [%d,%d]!\n", ObtemJogadaX(jogada), ObtemJogadaY(jogada));
                     tabuleiro = MarcaPosicaoTabuleiro(tabuleiro, jogador.id, ObtemJogadaX(jogada), ObtemJogadaY(jogada));
                 }else {
                     printf("Posicao invalida (OCUPADA - [%d,%d] )!\n", ObtemJogadaX(jogada), ObtemJogadaY(jogada));
                 }
+            }else {
+                printf("Posicao invalida (OCUPADA - [%d,%d] )!\n", ObtemJogadaX(jogada), ObtemJogadaY(jogada));
             }
         }else {
             printf("Posicao invalida (FORA DO TABULEIRO - [%d %d] )!\n", ObtemJogadaX(jogada), ObtemJogadaY(jogada));
@@ -73,8 +82,8 @@ int VenceuJogador(tJogador jogador, tTabuleiro tabuleiro) {
         return 1;
     
     diagonalSecundaria = 1;
-    for(lds = 0, cds = (TAM_TABULEIRO - 1); lds < TAM_TABULEIRO, cds >= 0; lds++, cds--){
-        if (EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, lds, cds, jogador.id)) {
+    for(lds = 0, cds = (TAM_TABULEIRO - 1); lds < TAM_TABULEIRO && cds >= 0; lds++, cds--){
+        if (!(EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, lds, cds, jogador.id))) {
             diagonalSecundaria = 0;
             break;
         }
