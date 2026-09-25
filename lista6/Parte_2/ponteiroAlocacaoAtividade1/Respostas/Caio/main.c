5179dcdf41e443811c31c5c04ea3d123f2bc15f7
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-09-25 09:12:01 -0300

    Adicionando arquivo principal

--- lista6/Parte_2/ponteiroAlocacaoAtividade1/Respostas/Caio/main.c
+++ lista6/Parte_2/ponteiroAlocacaoAtividade1/Respostas/Caio/main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "jogo.h"
+
 int main() {
-    
+    tJogo *jogo;
+
+    jogo = CriaJogo();
+
+    do {
+        ComecaJogo(jogo);
+    }while(ContinuaJogo());
+
+    DestroiJOgo(jogo);
+
     return 0;
 }
